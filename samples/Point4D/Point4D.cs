fbe04f14cbfdd6a295a5f5cac94f71286a4baf9e
Dalton Solano dos Reis <dalton.reis@gmail.com>
2018-11-28 16:06:49 -0200

    Inclui e testei novas funções para sobrecarga de operadores.

--- samples/Point4D/Point4D.cs
+++ samples/Point4D/Point4D.cs
@@ -2,10 +2,35 @@ using System;
 
 namespace CG {
   internal class Point4D {
-    public Point4D() {
-      Console.WriteLine("Point4D...");      
+    double x;
+    double y;
+    double z;
+    double w;
+    public Point4D(double x=0.0, double y=0.0, double z=0.0) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+      this.w = 1;
+      Console.WriteLine(" .. w: "+this.w);      
     }
-        // public Point4D() => Console.WriteLine("Point4d");
+
+
+    // Operator overloaded
+    public static Point4D operator +(Point4D pto1, Point4D pto2) => new Point4D(pto1.X + pto2.X, pto1.Y + pto2.Y, pto1.Z + pto2.Z);
+
+    public static Point4D operator --(Point4D pto) => new Point4D(-pto.X, -pto.Y, -pto.Z);
+
+    public static bool operator ==(Point4D pto1, Point4D pto2) {
+      return ((pto1.X == pto2.X) && (pto1.Y == pto2.Y) && (pto1.Z == pto2.Z));
+    }
+    public static bool operator !=(Point4D pto1, Point4D pto2) {
+      return ((pto1.X != pto2.X) && (pto1.Y != pto2.Y) && (pto1.Z != pto2.Z));
+    }
+
+
+    public double X { get => x; set => x = value; }
+    public double Y { get => y; set => y = value; }
+    public double Z { get => z; set => z = value; }
 
   }
 }
--- samples/Point4D/Program.cs
+++ samples/Point4D/Program.cs
@@ -6,10 +6,24 @@ namespace CG
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
-      Point4D ponto = new Point4D();
+      Point4D pto1 = new Point4D(10,20,30);
+      Console.WriteLine("PTO1 ... x: "+pto1.X+" - y: "+pto1.Y + " - z: "+pto1.Z);
 
-    //   var ponto = new Point4D();
+      Point4D pto2 = new Point4D(10,20,30);
+      Console.WriteLine("PTO2 ... x: "+pto2.X+" - y: "+pto2.Y + " - z: "+pto2.Z);
+      
+      if (pto1 == pto2)
+        Console.WriteLine("true");
+      else
+        Console.WriteLine("false");
+
+      pto2 += pto1;
+      Console.WriteLine("PTO ... x: "+pto2.X+" - y: "+pto2.Y + " - z: "+pto2.Z);
+
+      var pto = new Point4D(1,-2,3);
+      pto--;
+      Console.WriteLine("PTO ... x: "+pto.X+" - y: "+pto.Y + " - z: "+pto.Z);
+
     }
   }
 }
