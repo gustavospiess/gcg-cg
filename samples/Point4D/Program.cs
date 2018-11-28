e9cb9ba23bd6c5bf6e7873008b2091f05dde88f6
Dalton Solano dos Reis <dalton.reis@gmail.com>
2018-11-28 11:09:06 -0200

    Criei um projeto para testar a Classe Point4D.

---
+++ samples/Point4D/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CG
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Point4D ponto = new Point4D();
+
+    //   var ponto = new Point4D();
+    }
+  }
+}
