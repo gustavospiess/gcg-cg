b4f0f77c2e5edc966445ce0186fd66fffbe26476
Dalton Solano dos Reis <dalton.reis@gmail.com>
2018-11-29 11:17:29 -0200

    Renomei o NameSpace para GCG. Inclui código para usar o OpenTK. Agora desenha SRU3D e 4 pontos.

--- samples/Point4D/Point4D.cs
+++ samples/Point4D/Point4D.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace CG {
+namespace GCG {
   internal class Point4D {
     double x;
     double y;
@@ -11,7 +11,6 @@ namespace CG {
       this.y = y;
       this.z = z;
       this.w = 1;
-      Console.WriteLine(" .. w: "+this.w);      
     }
 
 
@@ -20,14 +19,12 @@ namespace CG {
 
     public static Point4D operator --(Point4D pto) => new Point4D(-pto.X, -pto.Y, -pto.Z);
 
-    public static bool operator ==(Point4D pto1, Point4D pto2) {
-      return ((pto1.X == pto2.X) && (pto1.Y == pto2.Y) && (pto1.Z == pto2.Z));
-    }
-    public static bool operator !=(Point4D pto1, Point4D pto2) {
-      return ((pto1.X != pto2.X) && (pto1.Y != pto2.Y) && (pto1.Z != pto2.Z));
-    }
-
-
+    // public static bool operator ==(Point4D pto1, Point4D pto2) {
+    //   return ((pto1.X == pto2.X) && (pto1.Y == pto2.Y) && (pto1.Z == pto2.Z));
+    // }
+    // public static bool operator !=(Point4D pto1, Point4D pto2) {
+    //   return ((pto1.X != pto2.X) && (pto1.Y != pto2.Y) && (pto1.Z != pto2.Z));
+    // }
     public double X { get => x; set => x = value; }
     public double Y { get => y; set => y = value; }
     public double Z { get => z; set => z = value; }
--- samples/Point4D/Program.cs
+++ samples/Point4D/Program.cs
@@ -1,28 +1,34 @@
 ﻿using System;
 
-namespace CG
+namespace GCG
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Point4D pto1 = new Point4D(10,20,30);
-      Console.WriteLine("PTO1 ... x: "+pto1.X+" - y: "+pto1.Y + " - z: "+pto1.Z);
+      Game window = new Game(800, 800);
+      window.Run();
 
-      Point4D pto2 = new Point4D(10,20,30);
-      Console.WriteLine("PTO2 ... x: "+pto2.X+" - y: "+pto2.Y + " - z: "+pto2.Z);
-      
+
+
+      Point4D pto1 = new Point4D(10, 20, 30);
+      Console.WriteLine("PTO1 ... x: " + pto1.X + " - y: " + pto1.Y + " - z: " + pto1.Z);
+
+      Point4D pto2 = new Point4D(10, 20, 30);
+      Console.WriteLine("PTO2 ... x: " + pto2.X + " - y: " + pto2.Y + " - z: " + pto2.Z);
+
       if (pto1 == pto2)
         Console.WriteLine("true");
       else
         Console.WriteLine("false");
 
       pto2 += pto1;
-      Console.WriteLine("PTO ... x: "+pto2.X+" - y: "+pto2.Y + " - z: "+pto2.Z);
+      Console.WriteLine("PTO ... x: " + pto2.X + " - y: " + pto2.Y + " - z: " + pto2.Z);
 
-      var pto = new Point4D(1,-2,3);
+      var pto = new Point4D(1, -2, 3);
       pto--;
-      Console.WriteLine("PTO ... x: "+pto.X+" - y: "+pto.Y + " - z: "+pto.Z);
+      Console.WriteLine("PTO Negativo ... x: " + pto.X + " - y: " + pto.Y + " - z: " + pto.Z);
+
 
     }
   }
