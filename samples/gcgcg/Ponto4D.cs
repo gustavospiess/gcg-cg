b2c102ea17ddb77c39d8c149d34b50b3d70b32cb
Dalton Solano dos Reis <dalton.reis@gmail.com>
2019-03-22 00:15:40 -0300

    Arrumei a formatação. Acrescentei comentários.

--- samples/gcgcg/Ponto4D.cs
+++ samples/gcgcg/Ponto4D.cs
@@ -1,23 +1,25 @@
 namespace gcgcg
 {
-  internal class Ponto4D {
-    double x;
-    double y;
-    double z;
-    double w;
-    public Ponto4D(double x=0.0, double y=0.0, double z=0.0) {
+  internal class Ponto4D
+  {
+    private double x;
+    private double y;
+    private double z;
+    private double w;
+    public Ponto4D(double x = 0.0, double y = 0.0, double z = 0.0)
+    {
       this.x = x;
       this.y = y;
       this.z = z;
       this.w = 1;
     }
-    
+
     // Operator overloaded
     public static Ponto4D operator +(Ponto4D pto1, Ponto4D pto2) => new Ponto4D(pto1.X + pto2.X, pto1.Y + pto2.Y, pto1.Z + pto2.Z);
 
     public static Ponto4D operator --(Ponto4D pto) => new Ponto4D(-pto.X, -pto.Y, -pto.Z);
 
-//TODO: Testar estas funções e ver se precisam existir
+    //TODO: Testar estas funções e ver se precisam existir
     // public static bool operator ==(Ponto4D pto1, Ponto4D pto2) {
     //   return ((pto1.X == pto2.X) && (pto1.Y == pto2.Y) && (pto1.Z == pto2.Z));
     // }
