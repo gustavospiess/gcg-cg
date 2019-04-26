39839a84a1a983c667163de9daaf24c6dd827822
Dalton Solano dos Reis <dalton.reis@gmail.com>
2019-04-26 15:25:52 -0300

    Só um projeto de teste.

---
+++ testes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
