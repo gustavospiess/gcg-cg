ee7f9b8bb24e78afa05e094c55385da0d91d12dd
Dalton Solano dos Reis <dalton.reis@gmail.com>
2019-05-10 00:19:21 -0300

    Copie os fontes do CG-N2 para desenvolver o CG-N3.

---
+++ CG-N3/Mundo.cs
@@ -0,0 +1,90 @@
+using System;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using OpenTK.Input;
+
+namespace gcgcg
+{
+  class Mundo
+  {
+    private Objeto objeto_1 = new Objeto();
+    private Objeto objeto_2 = new Objeto();
+    public Mundo()
+    {
+    }
+
+    public void Desenha()
+    {
+      // Console.WriteLine("[6] .. Desenha");
+
+      SRU3D();
+
+      objeto_1.Desenha();
+      objeto_2.Desenha();
+    }
+    //FIXME: não está considerando o NDC
+    public void MouseMove(int x, int y)
+    {
+      //* Invertendo a coordenada y do espaço de tela para o espaço do mundo */
+      objeto_1.Move(x, 600 - y);
+    }
+
+    public void OnKeyDown(OpenTK.Input.KeyboardKeyEventArgs e)
+    {
+      if (e.Key == Key.M)
+        objeto_2.exibeMatriz();
+      else
+        if (e.Key == Key.P)
+        objeto_2.exibePontos();
+      else
+        if (e.Key == Key.R)
+        objeto_2.atribuirIdentidade();
+      else
+        if (e.Key == Key.Left)
+        objeto_2.translacaoXYZ(-10, 0, 0);
+      else
+        if (e.Key == Key.Right)
+        objeto_2.translacaoXYZ(10, 0, 0);
+      else
+        if (e.Key == Key.Up)
+        objeto_2.translacaoXYZ(0, 10, 0);
+      else
+        if (e.Key == Key.Down)
+        objeto_2.translacaoXYZ(0, -10, 0);
+        else
+        if (e.Key == Key.PageUp)
+        objeto_2.escalaXYZ(2,2);
+        else
+        if (e.Key == Key.PageDown)
+        objeto_2.escalaXYZ(0.5,0.5);
+        else
+        if (e.Key == Key.Home)
+        objeto_2.escalaXYZPtoFixo(0.5,new CG_Biblioteca.Ponto4D(-150,-150));
+        else
+        if (e.Key == Key.End)
+        objeto_2.escalaXYZPtoFixo(2,new CG_Biblioteca.Ponto4D(-150,-150));
+        else
+        if (e.Key == Key.Number1)
+        objeto_2.rotacaoZPtoFixo(10,new CG_Biblioteca.Ponto4D(-150,-150));
+        else
+        if (e.Key == Key.Number2)
+        objeto_2.rotacaoZPtoFixo(-10,new CG_Biblioteca.Ponto4D(-150,-150));
+    }
+
+    private void SRU3D()
+    {
+      // Console.WriteLine("[5] .. SRU3D");
+
+      GL.LineWidth(1);
+      GL.Begin(PrimitiveType.Lines);
+      GL.Color3(Color.Red);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(200, 0, 0);
+      GL.Color3(Color.Green);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 200, 0);
+      GL.Color3(Color.Blue);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 0, 200);
+      GL.End();
+    }
+  }
+
+}
---
+++ CG-N3/Objeto.cs
@@ -0,0 +1,119 @@
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using CG_Biblioteca;
+using System;
+
+namespace gcgcg
+{
+  internal class Objeto
+  {
+    private Ponto4D[] listaPto = {
+      new Ponto4D(100, 100),new Ponto4D(200, 200) };
+    private BBox bBox = new BBox();
+    private Transformacao4D matriz = new Transformacao4D();
+
+    /// Matrizes temporarias que sempre sao inicializadas com matriz Identidade entao podem ser "static".
+    private static Transformacao4D matrizTmpTranslacao = new Transformacao4D();
+    private static Transformacao4D matrizTmpTranslacaoInversa = new Transformacao4D();
+    private static Transformacao4D matrizTmpEscala = new Transformacao4D();
+    private static Transformacao4D matrizGlobal = new Transformacao4D();
+
+    public Objeto()
+    {
+      atualizarBBox();
+    }
+    public void Desenha()
+    {
+      GL.LineWidth(2);
+      GL.Color3(Color.Cyan);
+
+      GL.PushMatrix();
+      GL.MultMatrix(matriz.GetDate());
+
+      GL.Begin(PrimitiveType.Lines);
+      GL.Vertex2(listaPto[0].X, listaPto[0].Y);
+      GL.Vertex2(listaPto[1].X, listaPto[1].Y);
+      GL.End();
+
+      //////////// ATENCAO: chamar desenho dos filhos... 
+
+      GL.PopMatrix();
+
+      bBox.desenhaBBox();
+    }
+    private void atualizarBBox()
+    {
+      bBox.atribuirBBox(listaPto[0]);
+      bBox.atualizarBBox(listaPto[1]);
+      bBox.processarCentroBBox();
+    }
+    public void Move(int x, int y)
+    {
+      listaPto[1].X = x;
+      listaPto[1].Y = y;
+      atualizarBBox();
+      // Console.WriteLine(" ..x: " + x);
+      // Console.WriteLine(" ..y: " + y);
+    }
+    public void exibeMatriz()
+    {
+      matriz.exibeMatriz();
+    }
+    public void exibePontos()
+    {
+      Console.WriteLine("P0[" + listaPto[0].X + "," + listaPto[0].Y + "," + listaPto[0].Z + "," + listaPto[0].W + "]");
+      Console.WriteLine("P1[" + listaPto[1].X + "," + listaPto[1].Y + "," + listaPto[1].Z + "," + listaPto[1].W + "]");
+    }
+    public void atribuirIdentidade()
+    {
+      matriz.atribuirIdentidade();
+    }
+    public void translacaoXYZ(double tx, double ty, double tz)
+    {
+      Transformacao4D matrizTranslate = new Transformacao4D();
+      matrizTranslate.atribuirTranslacao(tx, ty, tz);
+      matriz = matrizTranslate.transformMatrix(matriz);
+    }
+    public void escalaXYZ(double Sx, double Sy)
+    {
+      Transformacao4D matrizScale = new Transformacao4D();
+      matrizScale.atribuirEscala(Sx, Sy, 1.0);
+      matriz = matrizScale.transformMatrix(matriz);
+    }
+
+    public void escalaXYZPtoFixo(double escala, Ponto4D ptoFixo)
+    {
+      matrizGlobal.atribuirIdentidade();
+
+      matrizTmpTranslacao.atribuirTranslacao(ptoFixo.X, ptoFixo.Y, ptoFixo.Z);
+      matrizGlobal = matrizTmpTranslacao.transformMatrix(matrizGlobal);
+
+      matrizTmpEscala.atribuirEscala(escala, escala, 1.0);
+      matrizGlobal = matrizTmpEscala.transformMatrix(matrizGlobal);
+
+      ptoFixo.inverterSinal();
+      matrizTmpTranslacaoInversa.atribuirTranslacao(ptoFixo.X, ptoFixo.Y, ptoFixo.Z);
+      matrizGlobal = matrizTmpTranslacaoInversa.transformMatrix(matrizGlobal);
+
+      matriz = matriz.transformMatrix(matrizGlobal);
+    }
+    public void rotacaoZPtoFixo(double angulo, Ponto4D ptoFixo)
+    {
+      matrizGlobal.atribuirIdentidade();
+
+      matrizTmpTranslacao.atribuirTranslacao(ptoFixo.X, ptoFixo.Y, ptoFixo.Z);
+      matrizGlobal = matrizTmpTranslacao.transformMatrix(matrizGlobal);
+
+      matrizTmpEscala.atribuirRotacaoZ(Transformacao4D.DEG_TO_RAD * angulo);
+      matrizGlobal = matrizTmpEscala.transformMatrix(matrizGlobal);
+
+      ptoFixo.inverterSinal();
+      matrizTmpTranslacaoInversa.atribuirTranslacao(ptoFixo.X, ptoFixo.Y, ptoFixo.Z);
+      matrizGlobal = matrizTmpTranslacaoInversa.transformMatrix(matrizGlobal);
+
+      matriz = matriz.transformMatrix(matrizGlobal);
+    }
+
+
+  }
+}
