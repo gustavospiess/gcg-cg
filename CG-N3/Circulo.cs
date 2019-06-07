4c3cef10bc60f4aee678b6acd65c3543a0494b5b
Dalton Solano dos Reis <dalton.reis@gmail.com>
2019-06-07 11:17:28 -0300

    Adicionei atributo da Primitiva.

--- CG-N3/Circulo.cs
+++ CG-N3/Circulo.cs
@@ -2,17 +2,19 @@
   Autor: Dalton Solano dos Reis
  */
 
+using OpenTK.Graphics.OpenGL;
 using CG_Biblioteca;
 
 namespace gcgcg
 {
   //TODO: melhorar o exemplo de uso de herança (ver exemplo em: https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/classes-and-structs/inheritance)
-  //FIXME: não está desenahando certo o círculo
   internal class Circulo : Objeto
   {
     private double raio;
     private Ponto4D ptoCentro = new Ponto4D();
-    //TODO: inicailizar como atributo linestrip
+
+//TODO: usar atributo primitiva no método Desenha()
+    private PrimitiveType primitiva = PrimitiveType.LineLoop;
 
     public Circulo(Ponto4D ptoCentro, double raio) {
       this.ptoCentro = ptoCentro;
@@ -30,5 +32,6 @@ namespace gcgcg
       }
 
     }
+    public PrimitiveType Primitiva { get => primitiva; set => primitiva = value; }
   }
 }
