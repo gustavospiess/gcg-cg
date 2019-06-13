fa80e78eb95f873a3563f580f19bb5ea919cddd1
Dalton Solano dos Reis <dalton.reis@gmail.com>
2019-06-13 17:52:23 -0300

    Textura: exemplo
    Copia o exemplo criado no repositório privado.

---
+++ CG-N4_exemplos/textura/Program.cs
@@ -0,0 +1,164 @@
+﻿/**
+Fonte: https://github.com/mono/opentk/blob/master/Source/Examples/OpenGL/1.x/Textures.cs
+ */
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace textura
+{
+  class Render : GameWindow
+  {
+    //FIXME: precisei instalar $ brew install mono-libgdiplus
+    Vector3 eye = Vector3.Zero, target = Vector3.Zero, up = Vector3.UnitY;
+    Bitmap bitmap = new Bitmap("logoGCG.png");
+
+    int texture;
+
+    public Render(int width, int height) : base(width, height) { }
+
+    protected override void OnLoad(EventArgs e)
+    {
+      base.OnLoad(e);
+      GL.ClearColor(Color.Gray);
+      GL.Enable(EnableCap.DepthTest);
+      eye.X = eye.Y = eye.Z = 5;
+
+      GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+      GL.GenTextures(1, out texture);
+      GL.BindTexture(TextureTarget.Texture2D, texture);
+      GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+      GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+      BitmapData data = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+          ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+      GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
+          OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+      bitmap.UnlockBits(data);
+    }
+
+    protected override void OnUnload(EventArgs e)
+    {
+      GL.DeleteTextures(1, ref texture);
+    }
+
+    protected override void OnResize(EventArgs e)
+    {
+      base.OnResize(e);
+      // GL.Viewport(0, 0, Width, Height);
+      // GL.MatrixMode(MatrixMode.Projection);
+      // GL.LoadIdentity();
+      // GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+      GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+      Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1.0f, 50.0f);
+      GL.MatrixMode(MatrixMode.Projection);
+      GL.LoadMatrix(ref projection);
+    }
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+      base.OnUpdateFrame(e);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+      // GL.Clear(ClearBufferMask.ColorBufferBit);
+      // GL.MatrixMode(MatrixMode.Modelview);
+      // GL.LoadIdentity();
+
+      GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit); // GL.Clear(ClearBufferMask.ColorBufferBit);
+      Matrix4 modelview = Matrix4.LookAt(eye, target, up);
+      GL.MatrixMode(MatrixMode.Modelview);
+      GL.LoadMatrix(ref modelview);
+
+      SRU3D();
+
+      GL.Color3(Color.White);
+      GL.Enable(EnableCap.Texture2D);
+      GL.BindTexture(TextureTarget.Texture2D, texture);
+      GL.Begin(PrimitiveType.Quads);
+
+      // Face da frente
+      GL.Normal3(0, 0, 1);
+      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, 1.0f);
+      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, -1.0f, 1.0f);
+      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, 1.0f);
+      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, 1.0f, 1.0f);
+      // Face do fundo
+      GL.Normal3(0, 0, -1);
+      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
+      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
+      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, -1.0f);
+      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
+      // Face de cima
+      GL.Normal3(0, 1, 0);
+      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
+      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, 1.0f);
+      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, 1.0f);
+      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, -1.0f);
+      // Face de baixo
+      GL.Normal3(0, -1, 0);
+      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
+      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
+      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, 1.0f);
+      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, 1.0f);
+      // Face da direita
+      GL.Normal3(1, 0, 0);
+      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
+      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, 1.0f, -1.0f);
+      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, 1.0f);
+      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, 1.0f);
+      // Face da esquerda
+      GL.Normal3(-1, 0, 0);
+      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
+      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, 1.0f);
+      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(-1.0f, 1.0f, 1.0f);
+      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
+
+      GL.End();
+      GL.Disable(EnableCap.Texture2D);
+
+      SwapBuffers();
+    }
+
+    protected override void OnKeyDown(OpenTK.Input.KeyboardKeyEventArgs e)
+    {
+      if (e.Key == Key.Escape)
+        this.Exit();
+    }
+
+    protected override void OnMouseMove(MouseMoveEventArgs e)
+    {
+    }
+
+    private void SRU3D()
+    {
+      GL.LineWidth(3);
+      GL.Begin(PrimitiveType.Lines);
+      GL.Color3(Color.Red);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(200, 0, 0);
+      GL.Color3(Color.Green);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 200, 0);
+      GL.Color3(Color.Blue);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 0, 200);
+      GL.End();
+    }
+
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Render window = new Render(600, 600);
+      window.Run();
+      window.Run(1.0 / 60.0);
+    }
+  }
+
+}
