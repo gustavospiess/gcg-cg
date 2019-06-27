db6d27ff9d3455a84a597c2865bcc0348f598d98
Dalton Solano dos Reis <dalton.reis@gmail.com>
2019-06-27 20:19:51 -0300

    Ajustes da orientação anti-horária dos verticeis (CullFace).

--- CG-N4/Cubo.cs
+++ CG-N4/Cubo.cs
@@ -11,58 +11,76 @@ namespace gcgcg
     private bool exibeVetorNormal = false;
     public Cubo()
     {
+      listaPto.Add(new Ponto4D(-1, -1, 1)); // PtoA listaPto[0]
+      listaPto.Add(new Ponto4D(1, -1, 1)); // PtoB listaPto[1]
+      listaPto.Add(new Ponto4D(1, 1, 1)); // PtoC listaPto[2]
+      listaPto.Add(new Ponto4D(-1, 1, 1)); // PtoD listaPto[3]
+      listaPto.Add(new Ponto4D(-1, -1, -1)); // PtoE listaPto[4]
+      listaPto.Add(new Ponto4D(1, -1, -1)); // PtoF listaPto[5]
+      listaPto.Add(new Ponto4D(1, 1, -1)); // PtoG listaPto[6]
+      listaPto.Add(new Ponto4D(-1, 1, -1)); // PtoH listaPto[7]
+      atualizarBBox();
     }
 
-    //TODO: entender o uso da keyword new ... e replicar para os outros projetos
-    new public void Desenha()
+    public new void Desenha()
     {
-      GL.Begin(PrimitiveType.Quads);
-      // Face da frente
-      GL.Color3(1.0, 0.0, 0.0);
-      GL.Normal3(0, 0, 1);
-      GL.Vertex3(-1.0f, -1.0f, 1.0f);
-      GL.Vertex3(1.0f, -1.0f, 1.0f);
-      GL.Vertex3(1.0f, 1.0f, 1.0f);
-      GL.Vertex3(-1.0f, 1.0f, 1.0f);
-      // Face do fundo
-      GL.Color3(0.0, 1.0, 0.0);
-      GL.Normal3(0, 0, -1);
-      GL.Vertex3(-1.0f, -1.0f, -1.0f);
-      GL.Vertex3(-1.0f, 1.0f, -1.0f);
-      GL.Vertex3(1.0f, 1.0f, -1.0f);
-      GL.Vertex3(1.0f, -1.0f, -1.0f);
-      // Face de cima
-      GL.Color3(0.0, 0.0, 1.0);
-      GL.Normal3(0, 1, 0);
-      GL.Vertex3(-1.0f, 1.0f, -1.0f);
-      GL.Vertex3(-1.0f, 1.0f, 1.0f);
-      GL.Vertex3(1.0f, 1.0f, 1.0f);
-      GL.Vertex3(1.0f, 1.0f, -1.0f);
-      // Face de baixo
-      GL.Color3(1.0, 1.0, 0.0);
-      GL.Normal3(0, -1, 0);
-      GL.Vertex3(-1.0f, -1.0f, -1.0f);
-      GL.Vertex3(1.0f, -1.0f, -1.0f);
-      GL.Vertex3(1.0f, -1.0f, 1.0f);
-      GL.Vertex3(-1.0f, -1.0f, 1.0f);
-      // Face da direita
-      GL.Color3(0.0, 1.0, 1.0);
-      GL.Normal3(1, 0, 0);
-      GL.Vertex3(1.0f, -1.0f, -1.0f);
-      GL.Vertex3(1.0f, 1.0f, -1.0f);
-      GL.Vertex3(1.0f, 1.0f, 1.0f);
-      GL.Vertex3(1.0f, -1.0f, 1.0f);
-      // Face da esquerda
-      GL.Color3(1.0, 0.0, 1.0);
-      GL.Normal3(-1, 0, 0);
-      GL.Vertex3(-1.0f, -1.0f, -1.0f);
-      GL.Vertex3(-1.0f, -1.0f, 1.0f);
-      GL.Vertex3(-1.0f, 1.0f, 1.0f);
-      GL.Vertex3(-1.0f, 1.0f, -1.0f);
-      GL.End();
+      base.Desenha();
+
+      if (base.pegaExibeObjeto)
+      {
+        GL.PushMatrix();
+        GL.MultMatrix(matriz.GetDate());
+
+        GL.Begin(PrimitiveType.Quads);
+        // // Face da frente
+        GL.Color3(1.0, 0.0, 0.0);
+        GL.Normal3(0, 0, 1);
+        GL.Vertex3(listaPto[0].X, listaPto[0].Y, listaPto[0].Z);    // PtoA
+        GL.Vertex3(listaPto[1].X, listaPto[1].Y, listaPto[1].Z);    // PtoB
+        GL.Vertex3(listaPto[2].X, listaPto[2].Y, listaPto[2].Z);    // PtoC
+        GL.Vertex3(listaPto[3].X, listaPto[3].Y, listaPto[3].Z);    // PtoD
+        // Face do fundo
+        GL.Color3(0.0, 1.0, 0.0);
+        // GL.Normal3(0, 0, -1);
+        GL.Vertex3(listaPto[4].X, listaPto[4].Y, listaPto[4].Z);    // PtoE
+        GL.Vertex3(listaPto[7].X, listaPto[7].Y, listaPto[7].Z);    // PtoH
+        GL.Vertex3(listaPto[6].X, listaPto[6].Y, listaPto[6].Z);    // PtoG
+        GL.Vertex3(listaPto[5].X, listaPto[5].Y, listaPto[5].Z);    // PtoF
+        // Face de cima
+        GL.Color3(0.0, 0.0, 1.0);
+        GL.Normal3(0, 1, 0);
+        GL.Vertex3(listaPto[3].X, listaPto[3].Y, listaPto[3].Z);    // PtoD
+        GL.Vertex3(listaPto[2].X, listaPto[2].Y, listaPto[2].Z);    // PtoC
+        GL.Vertex3(listaPto[6].X, listaPto[6].Y, listaPto[6].Z);    // PtoG
+        GL.Vertex3(listaPto[7].X, listaPto[7].Y, listaPto[7].Z);    // PtoH
+        // Face de baixo
+        GL.Color3(1.0, 1.0, 0.0);
+        GL.Normal3(0, -1, 0);
+        GL.Vertex3(listaPto[0].X, listaPto[0].Y, listaPto[0].Z);    // PtoA
+        GL.Vertex3(listaPto[4].X, listaPto[4].Y, listaPto[4].Z);    // PtoE
+        GL.Vertex3(listaPto[5].X, listaPto[5].Y, listaPto[5].Z);    // PtoF
+        GL.Vertex3(listaPto[1].X, listaPto[1].Y, listaPto[1].Z);    // PtoB
+        // Face da direita
+        GL.Color3(0.0, 1.0, 1.0);
+        GL.Normal3(1, 0, 0);
+        GL.Vertex3(listaPto[1].X, listaPto[1].Y, listaPto[1].Z);    // PtoB
+        GL.Vertex3(listaPto[5].X, listaPto[5].Y, listaPto[5].Z);    // PtoF
+        GL.Vertex3(listaPto[6].X, listaPto[6].Y, listaPto[6].Z);    // PtoG
+        GL.Vertex3(listaPto[2].X, listaPto[2].Y, listaPto[2].Z);    // PtoC
+        // Face da esquerda
+        GL.Color3(1.0, 0.0, 1.0);
+        GL.Normal3(-1, 0, 0);
+        GL.Vertex3(listaPto[0].X, listaPto[0].Y, listaPto[0].Z);    // PtoA
+        GL.Vertex3(listaPto[3].X, listaPto[3].Y, listaPto[3].Z);    // PtoD
+        GL.Vertex3(listaPto[7].X, listaPto[7].Y, listaPto[7].Z);    // PtoH
+        GL.Vertex3(listaPto[4].X, listaPto[4].Y, listaPto[4].Z);    // PtoE
+        GL.End();
+      }
 
       if (exibeVetorNormal)
         ajudaExibirVetorNormal();
+
+      GL.PopMatrix();
     }
     public void ajudaExibirVetorNormal()
     {
