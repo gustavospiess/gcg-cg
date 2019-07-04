420616a4d46c84fe6be096cbf2ba23763a9de7c5
Dalton Solano dos Reis <dalton.reis@gmail.com>
2019-07-04 13:59:20 -0300

    Ajustei os valores das coordenadas dos vértices do cubo para fica com a mesma orientação (sentido anti-horário) do exemplo CG-N4

--- CG-N4_exemplos/textura/Program.cs
+++ CG-N4_exemplos/textura/Program.cs
@@ -86,39 +86,39 @@ namespace textura
       // Face da frente
       GL.Normal3(0, 0, 1);
       GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, 1.0f);
-      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, -1.0f, 1.0f);
-      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, 1.0f);
-      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, 1.0f, 1.0f);
+      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3( 1.0f, -1.0f, 1.0f);
+      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3( 1.0f,  1.0f, 1.0f);
+      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f,  1.0f, 1.0f);
       // Face do fundo
       GL.Normal3(0, 0, -1);
       GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
-      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
-      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, -1.0f);
-      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
+      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f,  1.0f, -1.0f);
+      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3( 1.0f,  1.0f, -1.0f);
+      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3( 1.0f, -1.0f, -1.0f);
       // Face de cima
       GL.Normal3(0, 1, 0);
-      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
-      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, 1.0f);
-      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, 1.0f);
-      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, -1.0f);
+      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f,  1.0f);
+      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3( 1.0f, 1.0f,  1.0f);
+      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3( 1.0f, 1.0f, -1.0f);
+      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
       // Face de baixo
       GL.Normal3(0, -1, 0);
-      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
-      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
-      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, 1.0f);
-      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, 1.0f);
+      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f,  1.0f);
+      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
+      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3( 1.0f, -1.0f, -1.0f);
+      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3( 1.0f, -1.0f,  1.0f);
       // Face da direita
       GL.Normal3(1, 0, 0);
-      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
-      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, 1.0f, -1.0f);
-      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, 1.0f);
-      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, 1.0f);
+      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(1.0f, -1.0f,  1.0f);
+      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
+      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f,  1.0f, -1.0f);
+      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f,  1.0f,  1.0f);
       // Face da esquerda
       GL.Normal3(-1, 0, 0);
-      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
-      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, 1.0f);
-      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(-1.0f, 1.0f, 1.0f);
-      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
+      GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f,  1.0f);
+      GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f,  1.0f,  1.0f);
+      GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(-1.0f,  1.0f, -1.0f);
+      GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
 
       GL.End();
       GL.Disable(EnableCap.Texture2D);
