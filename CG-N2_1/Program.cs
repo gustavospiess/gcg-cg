7534a92b0ed8039635ceaa596450f419b80420d1
Dalton Solano dos Reis <dalton.reis@gmail.com>
2019-08-29 11:26:27 -0300

    Ajustes de valores para melhorar a explicação. Acrescentei Sru2D.

--- CG-N2_1/Program.cs
+++ CG-N2_1/Program.cs
@@ -36,20 +36,32 @@ namespace OlaMundo
       base.OnUpdateFrame(e);
       GL.MatrixMode(MatrixMode.Projection);
       GL.LoadIdentity();
-      GL.Ortho(0, 600, 0, 600, 0, 600);
+      GL.Ortho(0, 600, 0, 600, -1, 1);
     }
     protected override void OnRenderFrame(FrameEventArgs e)
     {
       base.OnRenderFrame(e);
       GL.Clear(ClearBufferMask.ColorBufferBit);
       GL.MatrixMode(MatrixMode.Modelview);
-      GL.Color3(Color.Black);
+      GL.LoadIdentity();
+      Sru2D();
+      GL.Color3(Color.Yellow);
       GL.Begin(PrimitiveType.Lines);
-        GL.Vertex2(100, 100); GL.Vertex2(500, 500);
-        GL.Vertex2(100, 100); GL.Vertex2(500, 500);
+      GL.Vertex2(0, 0); GL.Vertex2(100, 100);
       GL.End();
       this.SwapBuffers();
     }
+    private void Sru2D()
+    {
+      GL.LineWidth(1);
+      GL.Begin(PrimitiveType.Lines);
+      GL.Color3(Color.Red);
+      GL.Vertex2(0, 0); GL.Vertex2(100, 0);
+      GL.Color3(Color.Green);
+      GL.Vertex2(0, 0); GL.Vertex2(0, 100);
+      GL.End();
+    }
+
   }
   class Program
   {
