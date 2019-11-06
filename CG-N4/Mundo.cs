3655d42c48c8b61674dc47ebc0fc81124d08a2fe
Dalton Solano dos Reis <dalton.reis@gmail.com>
2019-11-06 16:14:07 -0300

    Dupliquei o projeto todo CG-N3 para criar novas funções no CG-N4.

---
+++ CG-N4/Mundo.cs
@@ -0,0 +1,166 @@
+﻿#define CG_Gizmo
+#define CG_Privado
+
+using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using System.Collections.Generic;
+using OpenTK.Input;
+using CG_Biblioteca;
+
+namespace gcgcg
+{
+  class Mundo : GameWindow
+  {
+    private static Mundo instanciaMundo = null;
+
+    private Mundo(int width, int height) : base(width, height) { }
+
+    public static Mundo GetInstance(int width, int height)
+    {
+      if (instanciaMundo == null)
+        instanciaMundo = new Mundo(width, height);
+      return instanciaMundo;
+    }
+
+    private Camera camera = new Camera();
+    protected List<Objeto> objetosLista = new List<Objeto>();
+    private ObjetoAramado objetoSelecionado = null;
+    private bool bBoxDesenhar = false;
+    int mouseX, mouseY;   //FIXME: achar método MouseDown para não ter variável Global
+    private ObjetoAramado objetoNovo = null;
+    private String objetoId = "A";
+
+    protected override void OnLoad(EventArgs e)
+    {
+      base.OnLoad(e);
+      GL.ClearColor(Color.Gray);
+    }
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+      base.OnUpdateFrame(e);
+      GL.MatrixMode(MatrixMode.Projection);
+      GL.LoadIdentity();
+      GL.Ortho(camera.xmin, camera.xmax, camera.ymin, camera.ymax, camera.zmin, camera.zmax);
+    }
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+      base.OnRenderFrame(e);
+      GL.Clear(ClearBufferMask.ColorBufferBit);
+      GL.MatrixMode(MatrixMode.Modelview);
+      GL.LoadIdentity();
+#if CG_Gizmo      
+      Sru3D();
+#endif
+      for (var i = 0; i < objetosLista.Count; i++)
+        objetosLista[i].Desenhar();
+      if (bBoxDesenhar && (objetoSelecionado != null))
+        objetoSelecionado.BBox.Desenhar();
+      this.SwapBuffers();
+    }
+
+    protected override void OnKeyDown(OpenTK.Input.KeyboardKeyEventArgs e)
+    {
+      // N3-Exe2: usar o arquivo docs/umlClasses.wsd
+      // N3-Exe3: usar o arquivo bin/documentação.XML -> ver exemplo CG_Biblioteca/bin/documentação.XML
+      if (e.Key == Key.Escape)
+        Exit();
+      else if (e.Key == Key.E)    // N3-Exe4: ajuda a conferir se os poligonos e vértices estão certos
+      {
+        for (var i = 0; i < objetosLista.Count; i++)
+        {
+          objetosLista[i].PontosExibirObjeto();
+        }
+      }
+      else if (e.Key == Key.O)
+        bBoxDesenhar = !bBoxDesenhar;   // N3-Exe9: exibe a BBox
+      else if (e.Key == Key.M)
+        objetoSelecionado.ExibeMatriz();
+      else if (e.Key == Key.P)
+        objetoSelecionado.PontosExibirObjeto();
+      else if (e.Key == Key.I)
+        objetoSelecionado.AtribuirIdentidade();
+      //FIXME: não está atualizando a BBox com as transformações geométricas
+      else if (e.Key == Key.Left)
+        objetoSelecionado.TranslacaoXY(-10, 0);     // N3-Exe10: translação
+      else if (e.Key == Key.Right)
+        objetoSelecionado.TranslacaoXY(10, 0);      // N3-Exe10: translação
+      else if (e.Key == Key.Up)
+        objetoSelecionado.TranslacaoXY(0, 10);      // N3-Exe10: translação
+      else if (e.Key == Key.Down)
+        objetoSelecionado.TranslacaoXY(0, -10);     // N3-Exe10: translação
+      else if (e.Key == Key.PageUp)
+        objetoSelecionado.EscalaXY(2, 2);
+      else if (e.Key == Key.PageDown)
+        objetoSelecionado.EscalaXY(0.5, 0.5);
+      else if (e.Key == Key.Home)
+        objetoSelecionado.EscalaXYBBox(0.5);        // N3-Exe11: escala
+      else if (e.Key == Key.End)
+        objetoSelecionado.EscalaXYBBox(2);          // N3-Exe11: escala
+      else if (e.Key == Key.Number1)
+        objetoSelecionado.RotacaoZ(10);
+      else if (e.Key == Key.Number2)
+        objetoSelecionado.RotacaoZ(-10);
+      else if (e.Key == Key.Number3)
+        objetoSelecionado.RotacaoZBBox(10);         // N3-Exe12: rotação
+      else if (e.Key == Key.Number4)
+        objetoSelecionado.RotacaoZBBox(-10);        // N3-Exe12: rotação
+      else if (e.Key == Key.Enter)
+      {
+        objetoSelecionado = objetoNovo;
+        objetoNovo.PontosRemoverUltimo();   // N3-Exe6: "troque" para deixar o rastro
+        objetoNovo = null;
+      }
+      else if (e.Key == Key.Space)
+      {
+        if (objetoNovo == null)
+        {
+          objetoNovo = new ObjetoAramado(objetoId + 1, null);
+          objetosLista.Add(objetoNovo);
+          objetoNovo.PontosAdicionar(new Ponto4D(mouseX, mouseY));
+          objetoNovo.PontosAdicionar(new Ponto4D(mouseX, mouseY));  // N3-Exe6: "troque" para deixar o rastro
+        }
+        else
+          objetoNovo.PontosAdicionar(new Ponto4D(mouseX, mouseY));
+      }
+      else if (e.Key == Key.Number9)
+        objetoSelecionado = null;   //TODO: remover está tecla e atribuir o null qdo não tiver um poligono
+    }
+
+    //FIXME: não está considerando o NDC
+    protected override void OnMouseMove(MouseMoveEventArgs e)
+    {
+      mouseX = e.Position.X; mouseY = 600 - e.Position.Y; // Inverti eixo Y
+      if (objetoNovo != null)
+      {
+        objetoNovo.PontosUltimo().X = mouseX;             // N3-Exe5: movendo um vértice de um poligono específico
+        objetoNovo.PontosUltimo().Y = mouseY;
+      }
+    }
+
+#if CG_Gizmo
+    private void Sru3D()
+    {
+      GL.LineWidth(1);
+      GL.Begin(PrimitiveType.Lines);
+      GL.Color3(Color.Red);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(200, 0, 0);
+      GL.Color3(Color.Green);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 200, 0);
+      GL.Color3(Color.Blue);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 0, 200);
+      GL.End();
+    }
+#endif    
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Mundo window = Mundo.GetInstance(600, 600);
+      window.Title = "CG-N4";
+      window.Run(1.0 / 60.0);
+    }
+  }
+}
---
+++ CG-N4/ObjetoAramado.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using CG_Biblioteca;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace gcgcg
+{
+  internal class ObjetoAramado : Objeto
+  {
+    protected List<Ponto4D> pontosLista = new List<Ponto4D>();
+
+    public ObjetoAramado(string rotulo, Objeto paiRef) : base(rotulo, paiRef) { }
+
+    protected override void DesenharAramado()
+    {
+      GL.LineWidth(base.PrimitivaTamanho);
+      GL.Color3(Color.White);
+      GL.Begin(base.PrimitivaTipo);
+      foreach (Ponto4D pto in pontosLista)
+      {
+        GL.Vertex2(pto.X, pto.Y);
+      }
+      GL.End();
+    }
+
+    public void PontosAdicionar(Ponto4D pto)
+    {
+      pontosLista.Add(pto);
+      if (pontosLista.Count.Equals(1))
+        base.BBox.Atribuir(pto);
+      else
+        base.BBox.Atualizar(pto);
+      base.BBox.ProcessarCentro();
+    }
+
+    public void PontosRemoverUltimo()
+    {
+      pontosLista.RemoveAt(pontosLista.Count - 1);
+    }
+
+    protected void PontosRemoverTodos()
+    {
+      pontosLista.Clear();
+    }
+
+    public Ponto4D PontosUltimo()
+    {
+      return pontosLista[pontosLista.Count - 1];
+    }
+
+    protected override void PontosExibir()
+    {
+      Console.WriteLine("__ Objeto: " + base.rotulo);
+      for (var i = 0; i < pontosLista.Count; i++)
+      {
+        Console.WriteLine("P" + i + "[" + pontosLista[i].X + "," + pontosLista[i].Y + "," + pontosLista[i].Z + "," + pontosLista[i].W + "]");
+      }
+    }
+  }
+}
