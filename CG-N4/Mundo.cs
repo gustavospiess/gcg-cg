7c2a31dc76712fa824b75348ce1d1a498c1bec2d
Gustavo Henrique Spiess <gustavo.spiess@philips.com>
2020-07-12 21:11:16 -0300

    trabalho 4 v1

--- CG-N4/Mundo.cs
+++ CG-N4/Mundo.cs
@@ -3,7 +3,6 @@
 **/
 
 #define CG_Gizmo
-#define CG_Privado
 
 using System;
 using OpenTK;
@@ -40,6 +39,10 @@ namespace gcgcg
       return instanciaMundo;
     }
 
+    protected List<Transformacao4D> movimentos = new List<Transformacao4D>();
+    protected List<Int32> qtMovimentos = new List<Int32>();
+    protected int i;
+    protected int j;
     private CameraPerspective camera = new CameraPerspective();
     protected List<Objeto> objetosLista = new List<Objeto>();
     private ObjetoGeometria objetoSelecionado = null;
@@ -48,10 +51,6 @@ namespace gcgcg
     private Poligono objetoNovo = null;
     private String objetoId = "A";
     private Retangulo obj_Retangulo;
-#if CG_Privado
-    private Privado_SegReta obj_SegReta;
-    private Privado_Circulo obj_Circulo;
-#endif
     private Cubo obj_Cubo;
     private Cilindro obj_Cilindro;
     private Esfera obj_Esfera;
@@ -59,38 +58,70 @@ namespace gcgcg
 
     protected override void OnLoad(EventArgs e)
     {
+
+      this.pushMove(1, 0, 0, 3);
+
+      Transformacao4D tr = new Transformacao4D();
+      Transformacao4D trAux = new Transformacao4D();
+      trAux.AtribuirTranslacao(0, 0.3, 0);
+      tr.AtribuirRotacaoX(Math.PI/33);
+      this.movimentos.Add(trAux.MultiplicarMatriz(tr));
+      this.qtMovimentos.Add(33);
+      this.pushMove(1, 0, 0, 4);
+      this.movimentos.Add(trAux.MultiplicarMatriz(tr));
+      this.qtMovimentos.Add(33);
+      this.pushMove(1, 0, 0, 3);
+      
+
+      this.pushMove(0, 0, 1, 10);
+      this.pushMove(0, 1, 0, 10);
+      this.pushMove(0, 0, -1, 10);
+      this.pushMove(-1, 0, 0, 10);
+      this.pushMove(0, -1, 0, 5);
+
+      tr = new Transformacao4D();
+      trAux = new Transformacao4D();
+      trAux.AtribuirTranslacao(0.3, 0, 0);
+      tr.AtribuirRotacaoY(Math.PI/33);
+      this.movimentos.Add(trAux.MultiplicarMatriz(tr));
+      this.qtMovimentos.Add(33);
+
+      this.pushMove(0, 0, 1, 10);
+      this.pushMove(0, -1, 0, 5);
+      this.pushMove(1, 0, 0, 10);
+      this.pushMove(0, 0, -1, 10);
+      this.movimentos.Add(trAux.MultiplicarMatriz(tr));
+      this.qtMovimentos.Add(33);
+      this.pushMove(1, 0, 0, 10);
+
+      
       base.OnLoad(e);
       Console.WriteLine(" --- Ajuda / Teclas: ");
       Console.WriteLine(" [  H     ] mostra teclas usadas. ");
 
-      obj_Retangulo = new Retangulo("A", null, new Ponto4D(50, 50, 0), new Ponto4D(150, 150, 0));
-      objetosLista.Add(obj_Retangulo);
-      objetoSelecionado = obj_Retangulo;
+      // obj_Retangulo = new Retangulo("A", null, new Ponto4D(50, 50, 0), new Ponto4D(150, 150, 0));
+      // objetosLista.Add(obj_Retangulo);
+      // objetoSelecionado = obj_Retangulo;
 
-#if CG_Privado
-      obj_SegReta = new Privado_SegReta("B", null, new Ponto4D(50, 150), new Ponto4D(150, 250));
-      objetosLista.Add(obj_SegReta);
-      objetoSelecionado = obj_SegReta;
 
-      obj_Circulo = new Privado_Circulo("C", null, new Ponto4D(100, 300), 50);
-      objetosLista.Add(obj_Circulo);
-      objetoSelecionado = obj_Circulo;
-#endif
+      // obj_Cilindro = new Cilindro("D", null);
+      // objetosLista.Add(obj_Cilindro);
+      // obj_Cilindro.EscalaXYZ(50, 50, 50);
+      // obj_Cilindro.TranslacaoXYZ(150, 0, 0);
 
-      obj_Cilindro = new Cilindro("D", null);
-      objetosLista.Add(obj_Cilindro);
-      obj_Cilindro.EscalaXYZ(50, 50, 50);
-      obj_Cilindro.TranslacaoXYZ(150, 0, 0);
-
-      obj_Esfera = new Esfera("E", null);
-      objetosLista.Add(obj_Esfera);
-      obj_Esfera.EscalaXYZ(50, 50, 50);
-      obj_Esfera.TranslacaoXYZ(200, 0, 0);
+      // obj_Esfera = new Esfera("E", null);
+      // objetosLista.Add(obj_Esfera);
+      // obj_Esfera.EscalaXYZ(50, 50, 50);
+      // obj_Esfera.TranslacaoXYZ(200, 0, 0);
 
       // obj_Cone = new Cone("F", null);
       // objetosLista.Add(obj_Cone);
       // obj_Cone.EscalaXYZ(50, 50, 50);
       // obj_Cone.TranslacaoXYZ(250,0,0);
+
+      // obj_Cubo = new Cubo("G", null);
+      // objetosLista.Add(obj_Cubo);
+      // obj_Cubo.EscalaXYZ(50, 50, 50);
 
       obj_Cubo = new Cubo("G", null);
       objetosLista.Add(obj_Cubo);
@@ -100,8 +131,9 @@ namespace gcgcg
 
       camera.At = new Vector3(0, 0, 0);
       camera.Eye = new Vector3(1000, 1000, 1000);
+      // camera.Eye = new Vector3(0, 0, 1000);
       camera.Near = 100.0f;
-      camera.Far = 2000.0f;
+      camera.Far = 4000.0f;
 
       GL.ClearColor(127,127,127,255);
       GL.Enable(EnableCap.DepthTest);
@@ -123,6 +155,7 @@ namespace gcgcg
     {
       base.OnUpdateFrame(e);
     }
+
     protected override void OnRenderFrame(FrameEventArgs e)
     {
       base.OnRenderFrame(e);
@@ -137,6 +170,35 @@ namespace gcgcg
         objetosLista[i].Desenhar();
       if (bBoxDesenhar && (objetoSelecionado != null))
         objetoSelecionado.BBox.Desenhar();
+
+      Transformacao4D tr = new Transformacao4D();
+      tr.AtribuirEscala(50, 50, 50);
+      Ponto4D pt = new Ponto4D(0, 0, 0);
+
+      GL.LineWidth(5);
+      GL.Color3(0, 0, 0);
+      GL.Begin(PrimitiveType.LineLoop);
+      for (var i = 0; i < movimentos.Count; i++)
+      {
+        for (var j = 0; j < qtMovimentos[i]; j++)
+        {
+          Ponto4D pt2 = tr.MultiplicarPonto(pt);
+          GL.Vertex3(pt2.X, pt2.Y, pt2.Z);
+          tr = tr.MultiplicarMatriz(movimentos[i]);
+        }
+      }
+      GL.End();
+
+      objetoSelecionado.Matriz = objetoSelecionado.Matriz.MultiplicarMatriz(this.movimentos[i]);
+      j++;
+      if (j >= this.qtMovimentos[i])
+      {
+        i++;
+        j = 0;
+        if (i >= this.movimentos.Count)
+          i = 0;
+      }
+
       this.SwapBuffers();
     }
 
@@ -165,36 +227,45 @@ namespace gcgcg
           objetoNovo = null;
         }
       }
-      else if (e.Key == Key.Space)
+      else if (e.Key == Key.Left)
       {
-        if (objetoNovo == null)
-        {
-          objetoNovo = new Poligono(objetoId + 1, null);
-          objetosLista.Add(objetoNovo);
-          objetoNovo.PontosAdicionar(new Ponto4D(mouseX, mouseY));
-          objetoNovo.PontosAdicionar(new Ponto4D(mouseX, mouseY));  // N3-Exe6: "troque" para deixar o rastro
-        }
-        else
-          objetoNovo.PontosAdicionar(new Ponto4D(mouseX, mouseY));
+        this.AtRotateX(-1/2);
+        this.AtRotateY(1);
       }
+      else if (e.Key == Key.Right)
+      {
+        this.AtRotateX(1/2);
+        this.AtRotateY(-1);
+      }
+      else if (e.Key == Key.Up)
+        this.EyeRotate(1);
+      else if (e.Key == Key.Down)
+        this.EyeRotate(-1);
       else if (objetoSelecionado != null)
       {
-        if (e.Key == Key.M)
+        if (e.Key == Key.Space)
+        {
+          objetoSelecionado.Matriz = objetoSelecionado.Matriz.MultiplicarMatriz(this.movimentos[i]);
+          j++;
+          if (j >= this.qtMovimentos[i])
+          {
+            i++;
+            j = 0;
+            if (i >= this.movimentos.Count)
+              i = 0;
+          }
+        }
+        else if (e.Key == Key.M)
           Console.WriteLine(objetoSelecionado.Matriz);
         else if (e.Key == Key.P)
           Console.WriteLine(objetoSelecionado);
         else if (e.Key == Key.I)
+        {
           objetoSelecionado.AtribuirIdentidade();
-        //TODO: não está atualizando a BBox com as transformações geométricas
-        else if (e.Key == Key.Left)
-          objetoSelecionado.TranslacaoXYZ(-10, 0, 0);
-        else if (e.Key == Key.Right)
-          objetoSelecionado.TranslacaoXYZ(10, 0, 0);
-        else if (e.Key == Key.Up)
-          objetoSelecionado.TranslacaoXYZ(0, 10, 0);
-        else if (e.Key == Key.Down)
-          objetoSelecionado.TranslacaoXYZ(0, -10, 0);
-        else if (e.Key == Key.Number8)
+          this.i = 0;
+          this.j = 0;
+        }
+       else if (e.Key == Key.Number8)
           objetoSelecionado.TranslacaoXYZ(0, 0, 10);
         else if (e.Key == Key.Number9)
           objetoSelecionado.TranslacaoXYZ(0, 0, -10);
@@ -233,11 +304,60 @@ namespace gcgcg
     protected override void OnMouseMove(MouseMoveEventArgs e)
     {
       mouseX = e.Position.X; mouseY = 600 - e.Position.Y; // Inverti eixo Y
-      if (objetoNovo != null)
-      {
-        objetoNovo.PontosUltimo().X = mouseX;
-        objetoNovo.PontosUltimo().Y = mouseY;
-      }
+    }
+
+    private Vector3 PontoToVector(Ponto4D p)
+    {
+      return new Vector3(Convert.ToSingle(p.X), Convert.ToSingle(p.Y), Convert.ToSingle(p.Z));
+    }
+
+    private Ponto4D VectorToPonto(Vector3 v)
+    {
+      return new Ponto4D(v.X, v.Y, v.Z);
+    }
+
+    private void AtRotateY(float n)
+    {
+      Transformacao4D matA = new Transformacao4D();
+      matA.AtribuirTranslacao(camera.Eye.X, camera.Eye.Y, camera.Eye.Z);
+      Transformacao4D matB = new Transformacao4D();
+      matB.AtribuirRotacaoY(n * Math.PI/120);
+      matA = matA.MultiplicarMatriz(matB);
+      matB.AtribuirTranslacao(-1*camera.Eye.X, -1*camera.Eye.Y, -1*camera.Eye.Z);
+      matA = matA.MultiplicarMatriz(matB);
+      this.camera.At = PontoToVector(matA.MultiplicarPonto(VectorToPonto(this.camera.At)));
+    }
+
+    private void AtRotateX(float n)
+    {
+      Transformacao4D matA = new Transformacao4D();
+      matA.AtribuirTranslacao(camera.Eye.X, camera.Eye.Y, camera.Eye.Z);
+      Transformacao4D matB = new Transformacao4D();
+      matB.AtribuirRotacaoX(n * Math.PI/120);
+      matA = matA.MultiplicarMatriz(matB);
+      matB.AtribuirTranslacao(-1*camera.Eye.X, -1*camera.Eye.Y, -1*camera.Eye.Z);
+      matA = matA.MultiplicarMatriz(matB);
+      this.camera.At = PontoToVector(matA.MultiplicarPonto(VectorToPonto(this.camera.At)));
+    }
+
+    private void EyeRotate(float n)
+    {
+      Transformacao4D matA = new Transformacao4D();
+      matA.AtribuirTranslacao(camera.At.X, camera.At.Y, camera.At.Z);
+      Transformacao4D matB = new Transformacao4D();
+      matB.AtribuirRotacaoY(n * Math.PI/120);
+      matA = matA.MultiplicarMatriz(matB);
+      matB.AtribuirTranslacao(-1*camera.At.X, -1*camera.At.Y, -1*camera.At.Z);
+      matA = matA.MultiplicarMatriz(matB);
+      this.camera.Eye = PontoToVector(matA.MultiplicarPonto(VectorToPonto(this.camera.Eye)));
+    }
+
+    private void pushMove(float x, float y, float z, int qt)
+    {
+      Transformacao4D tr = new Transformacao4D();
+      tr.AtribuirTranslacao(x/3, y/3, z/3);
+      this.movimentos.Add(tr);
+      this.qtMovimentos.Add(qt*3);
     }
 
 #if CG_Gizmo
@@ -246,11 +366,11 @@ namespace gcgcg
       GL.LineWidth(1);
       GL.Begin(PrimitiveType.Lines);
       GL.Color3(255,0,0);
-      GL.Vertex3(0, 0, 0); GL.Vertex3(200, 0, 0);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(20000, 0, 0);
       GL.Color3(0,255,0);
-      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 200, 0);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 20000, 0);
       GL.Color3(0,0,255);
-      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 0, 200);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 0, 20000);
       GL.End();
     }
 #endif    
--- CG-N4/Objeto.cs
+++ CG-N4/Objeto.cs
@@ -20,7 +20,7 @@ namespace gcgcg
     private List<Objeto> objetosLista = new List<Objeto>();
 
     private Transformacao4D matriz = new Transformacao4D();
-    public Transformacao4D Matriz { get => matriz; }
+    public Transformacao4D Matriz { get => matriz; set => matriz = value;}
 
     /// Matrizes temporarias que sempre sao inicializadas com matriz Identidade entao podem ser "static".
     private static Transformacao4D matrizTmpTranslacao = new Transformacao4D();
